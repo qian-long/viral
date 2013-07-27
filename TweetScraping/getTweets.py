5183b12dbb6cac891cb2e44f466ed6f87d8eab5c
Harini Kannan <hkannan@mit.edu>
2013-07-27 03:46:52 -0400

    getTweets

--- TweetScraping/getTweets.py
+++ TweetScraping/getTweets.py
@@ -1,9 +1,9 @@
 import tweepy
 import json
 import calendar
-auth = tweepy.OAuthHandler("MTS6yOt6FMiPO9901Nzeg", "O92CIx5F11bwqmVj6unLLv4jvzwqe3E8eAPXCjAPlM")
+auth = tweepy.OAuthHandler("fmOVYZLqm0uSBkCS1tLCQ", "G94sqrzpCgcy7ZUEZxv5j8IMjgKG3gpVCpI4PqdH8")
 
-auth.set_access_token("45437141-GlEEbvfpgzy3jK5PexejHux3nVKR9J7Ra0DKoEFn9", "u98Oe57ibDoWEuoaCAgCtenFljspgaOnest2TXoExZU")
+auth.set_access_token("1467714804-Xjbb3fuTT6abBj6QIQasyVnJSJo7XXvkI6Rz9Yr", "c2lD0qQZQku99fdBDpRmVGyn0NVEVVkNq4tSNLvI59s")
 api = tweepy.API(auth)
 
 #tweets = api.search('', count=1000)
@@ -11,24 +11,28 @@ counter = 0
 output = []
 #print len(tweets)
 current_id = 360973835627020288
-while counter < 30:
+while counter < 31:
     tweets = api.search('royal baby', count=15, result_type='recent', max_id = current_id)
     for tweet in tweets:
         dict = {}
         counter = counter + 1
 #       print str(counter) + tweet.text + str(tweet.created_at)
+        dict["lat"] = None
+        dict["long"] = None
         dict["loc"] = None
-        if (tweet.coordinates is not None):
-            dict["lat"] = tweet.coordinates[1]
-            dict["long"] = tweet.coordinates[0]
-        elif (tweet.geo is not None):
-            dict["lat"] = tweet.geo[0]
-            dict["long"] = tweet.geo[1]
+        if tweet.coordinates is not None:
+            dict["lat"] = tweet.coordinates['coordinates'][1]
+            dict["long"] = tweet.coordinates['coordinates'][0]
+        elif tweet.geo is not None:
+            dict["lat"] = tweet.geo['coordinates'][0]
+            dict["long"] = tweet.geo['coordinates'][1]
+
         else:
             dict["lat"] = None
             dict["long"] = None
             if tweet.user.location is not None:
                 dict["loc"] = tweet.user.location                
+
         dict["ID"] = tweet.id
         dict["text"] = tweet.text
         timestamp = calendar.timegm(tweet.created_at.utctimetuple())
@@ -37,6 +41,7 @@ while counter < 30:
     current_id = tweets.since_id
 
 print output
+print "//" + str(current_id)
 
 '''
 tweets2 = api.search('asiana', count=100, result_type='recent', max_id=360932756148060160)
