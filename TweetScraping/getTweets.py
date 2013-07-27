6b9ce86b007dd4b8bd4e2a6f53c9901a7e8b467c
Harini Kannan <hkannan@mit.edu>
2013-07-27 02:58:37 -0400

    modified getTweets

--- TweetScraping/getTweets.py
+++ TweetScraping/getTweets.py
@@ -1,17 +1,49 @@
 import tweepy
+import json
+import calendar
+auth = tweepy.OAuthHandler("MTS6yOt6FMiPO9901Nzeg", "O92CIx5F11bwqmVj6unLLv4jvzwqe3E8eAPXCjAPlM")
 
-auth = tweepy.OAuthHandler("Dymgnrgon9xzUPiVaLdysw", "NbvrqBFFW9LVAqZ5Jd305EJOB2dNV2JAckYBu3EqB3o")
-
-auth.set_access_token("1467714804-03r0XdOXrq2Xi3OCTiLi4ztjaPzPkYSpq18LlBB", "hmWr6tshZ3TpGt9uEYCX8Wa32TfHSL25onegjjvHg")
+auth.set_access_token("45437141-GlEEbvfpgzy3jK5PexejHux3nVKR9J7Ra0DKoEFn9", "u98Oe57ibDoWEuoaCAgCtenFljspgaOnest2TXoExZU")
 api = tweepy.API(auth)
 
-tweets = api.search('asiana', count=1000)
+#tweets = api.search('', count=1000)
 counter = 0
-print len(tweets)
-for tweet in tweets:
-    counter = counter + 1
-    print str(counter) + tweet.text + tweet.id
+output = []
+#print len(tweets)
+current_id = 360973835627020288
+while counter < 30:
+    tweets = api.search('royal baby', count=15, result_type='recent', max_id = current_id)
+    for tweet in tweets:
+        dict = {}
+        counter = counter + 1
+#       print str(counter) + tweet.text + str(tweet.created_at)
+        dict["loc"] = None
+        if (tweet.coordinates is not None):
+            dict["lat"] = tweet.coordinates[1]
+            dict["long"] = tweet.coordinates[0]
+        elif (tweet.geo is not None):
+            dict["lat"] = tweet.geo[0]
+            dict["long"] = tweet.geo[1]
+        else:
+            dict["lat"] = None
+            dict["long"] = None
+            if tweet.user.location is not None:
+                dict["loc"] = tweet.user.location                
+        dict["ID"] = tweet.id
+        dict["text"] = tweet.text
+        timestamp = calendar.timegm(tweet.created_at.utctimetuple())
+        dict["time"] = timestamp
+        output.append(dict)
+    current_id = tweets.since_id
 
+print output
+
+'''
+tweets2 = api.search('asiana', count=100, result_type='recent', max_id=360932756148060160)
+for tweet in tweets2:
+    counter = counter + 1
+    print str(counter) + tweet.text + str(tweet.id)
+'''
 '''
 print api.me().name
 statuses = api.user_timeline()
